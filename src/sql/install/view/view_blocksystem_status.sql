560bacaab9b4497436c23bde25410a19fe5540e0
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-03-27 10:29:45 +0100

    Version bump to 1.0.8

--- src/sql/install/view/view_blocksystem_status.sql
+++ src/sql/install/view/view_blocksystem_status.sql
@@ -1,41 +1,8 @@
 DELIMITER ;
 
-CREATE OR REPLACE VIEW `view_blocksystem_status_blocks_wahlscheine` AS (
-    select
-        `wahlschein`.`id` AS `id`,
-        concat(
-            `wahlschein`.`blocknumber`,
-            `wahlschein`.`wahlscheinstatus`
-        ) AS `blocks_id`,
-        cast(`wahlschein`.`row_start` as date) AS `letzte_aenderung`,
-        `wahlschein`.`wahlscheinstatus` AS `wahlscheinstatus`,
-        `wahlschein`.`wahlscheinnummer` AS `wahlscheinnummer`,
-        `wahlberechtigte`.`identnummer` AS `identnummer`,
-        `wahlschein`.`blocknumber` AS `blocknumber`,
-        `wahlscheinstatus`.`name` AS `name`,
-        `stimmzettel`.`name` AS `stimmzettel`
-    from
-        (
-            (
-                (
-                    `wahlschein`
-                    join `wahlscheinstatus` on(
-                        `wahlschein`.`wahlscheinstatus` = `wahlscheinstatus`.`id`
-                    )
-                )
-                join `wahlberechtigte` on(
-                    `wahlschein`.`wahlberechtigte` = `wahlberechtigte`.`id`
-                )
-            )
-            join `stimmzettel` on(
-                `wahlschein`.`stimmzettel` = `stimmzettel`.`id`
-            )
-        )
-    where
-        `wahlschein`.`blocknumber` <> '0'
-);
-
-CREATE oR replace VIEW `view_blocksystem_status_blocks` AS (
+ 
+CREATE
+OR REPLACE VIEW `view_blocksystem_status_blocks` AS (
     select
         concat(
             `wahlschein`.`blocknumber`,
@@ -45,7 +12,9 @@ CREATE oR replace VIEW `view_blocksystem_status_blocks` AS (
         `wahlschein`.`blocknumber` AS `blocknumber`,
         `wahlscheinstatus`.`name` AS `name`,
         max(
-            cast(`wahlschein`.`row_start` as date)
+            cast(
+                `wahlschein`.`updated_at` as date
+            )
         ) AS `letzte_aenderung`,
         count(distinct `wahlschein`.`id`) AS `anzahl_wahlscheine`
     from
@@ -61,14 +30,20 @@ CREATE oR replace VIEW `view_blocksystem_status_blocks` AS (
         `wahlschein`.`wahlscheinstatus`,
         `wahlschein`.`blocknumber`
 );
-
-CREATE OR REPLACE VIEW `view_blocksystem_status` AS (
+ 
+CREATE OR REPLACE  VIEW `view_blocksystem_status` AS (
     select
         `wahlschein`.`wahlscheinstatus` AS `wahlscheinstatus`,
         `wahlscheinstatus`.`name` AS `name`,
-        cast(`wahlschein`.`row_start` as date) AS `letzte_aenderung`,
-        count(distinct `wahlschein`.`id`) AS `anzahl_wahlscheine`,
-        count(distinct `wahlschein`.`blocknumber`) AS `anzahl_blocknumber`
+        cast(
+            `wahlschein`.`updated_at` as date
+        ) AS `letzte_aenderung`,
+        count(
+            distinct `wahlschein`.`id`
+        ) AS `anzahl_wahlscheine`,
+        count(
+            distinct `wahlschein`.`blocknumber`
+        ) AS `anzahl_blocknumber`
     from
         (
             `wahlschein`
@@ -80,4 +55,4 @@ CREATE OR REPLACE VIEW `view_blocksystem_status` AS (
         `wahlschein`.`blocknumber` <> '0'
     group by
         `wahlschein`.`wahlscheinstatus`
-);
+)
